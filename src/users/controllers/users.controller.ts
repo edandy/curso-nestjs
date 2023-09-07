4e1fb6c9efa7d8512a42f63acce6c756e984dfa6
Dandy <edandy.perez@gmail.com>
2023-09-07 00:16:28 -0500

    Manejo de errores

--- src/users/controllers/users.controller.ts
+++ src/users/controllers/users.controller.ts
@@ -1,11 +1,43 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { UsersService } from '../services/users.service';
+import { UserDto, UserUpdateDto } from '../dto/user.dto';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
   @Get()
-  index() {
-    return this.usersService.index();
+  public async findAllUsers() {
+    return this.usersService.findUsers();
+  }
+
+  @Post('register')
+  public async registerUser(@Body() body: UserDto) {
+    return this.usersService.createUser(body);
+  }
+
+  @Get(':id')
+  public async getUserById(@Param('id') id: string) {
+    return this.usersService.findUsersById(id);
+  }
+
+  @Put(':id')
+  public async updateUser(
+    @Param('id') id: string,
+    @Body() body: UserUpdateDto,
+  ) {
+    return this.usersService.updateUser(body, id);
+  }
+
+  @Delete(':id')
+  public async deleteUser(@Param('id') id: string) {
+    return this.usersService.deleteUser(id);
   }
 }
--- src/users/services/users.service.ts
+++ src/users/services/users.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { UsersEntity } from '../entities/users.entity';
 import { DeleteResult, Repository, UpdateResult } from 'typeorm';
 import { UserDto, UserUpdateDto } from '../dto/user.dto';
+import { ErrorManager } from '../../utils/error.manager';
 
 @Injectable()
 export class UsersService {
@@ -24,20 +25,35 @@ export class UsersService {
 
   public async findUsers(): Promise<UsersEntity[]> {
     try {
-      return await this.userRepository.find();
+      const users: UsersEntity[] = await this.userRepository.find();
+      if (users.length === 0) {
+        throw new ErrorManager({
+          type: 'BAD_REQUEST',
+          message: 'No se encontraron resultados',
+        });
+      }
+      return users;
     } catch (error) {
-      throw new Error(error);
+      throw ErrorManager.createSignatureError(error.message);
     }
   }
 
   public async findUsersById(id: string): Promise<UsersEntity> {
     try {
-      return await this.userRepository
+      const user: UsersEntity = await this.userRepository
         .createQueryBuilder('user')
         .where({ id })
         .getOne();
+
+      if (!user) {
+        throw new ErrorManager({
+          type: 'BAD_REQUEST',
+          message: 'No se pudo actualizar',
+        });
+      }
+      return user;
     } catch (error) {
-      throw new Error(error);
+      throw ErrorManager.createSignatureError(error.message);
     }
   }
 
@@ -49,12 +65,15 @@ export class UsersService {
       const user: UpdateResult = await this.userRepository.update(id, body);
 
       if (user.affected === 0) {
-        return undefined;
+        throw new ErrorManager({
+          type: 'BAD_REQUEST',
+          message: 'No se pudo actualizar',
+        });
       }
 
       return user;
     } catch (error) {
-      throw new Error(error);
+      throw ErrorManager.createSignatureError(error.message);
     }
   }
 
@@ -63,12 +82,15 @@ export class UsersService {
       const user: DeleteResult = await this.userRepository.delete(id);
 
       if (user.affected === 0) {
-        return undefined;
+        throw new ErrorManager({
+          type: 'BAD_REQUEST',
+          message: 'No se pudo borrar',
+        });
       }
 
       return user;
     } catch (error) {
-      throw new Error(error);
+      throw ErrorManager.createSignatureError(error.message);
     }
   }
 }
